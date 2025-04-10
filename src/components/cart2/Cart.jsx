7cac44f3571da1627abd1e77ad61d95ecdefcbdb
Thai-Hieu <thaihieu20062006@gmail.com>
2025-04-10 11:11:19 +0700

    fix style  cartModal

--- src/components/cart2/Cart.jsx
+++ src/components/cart2/Cart.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Cart.css";
 
 const Cart = () => {
     const navigate = useNavigate();
